df9e2f104cb585f46577c6d6369e572b76d0c609
Adam Štefanides <xstefaa00@stud.fit.vutbr.cz>
2026-03-23 23:20:01 +0100

    add method

--- src/main/java/org/iis2024/Date.java
+++ src/main/java/org/iis2024/Date.java
@@ -19,6 +19,10 @@ public class Date {
   private final int day;
   private final int month;
   private final int year;
+	
+  public void methodAddedByOwner() {
+    System.out.println("Method added by Adam.");
+  }
 
   /**
    * Retrieves the day component of this date.
