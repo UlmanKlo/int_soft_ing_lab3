6af7200bea8b974344ad178ab3783b544d33a9e8
Marsello71 <koptakmarcel@gmail.com>
2026-03-23 23:48:16 +0100

    sync

--- src/main/java/org/iis2024/Date.java
+++ src/main/java/org/iis2024/Date.java
@@ -17,6 +17,7 @@ import java.text.SimpleDateFormat;
  * month, and year values cannot be changed.
  */
 public class Date {
+  private int collaborator;
   private final int day;
   private final int month;
   private final int year;
